41d5a22c44d59ddb5e7c189953b0175452f2bde8
Bruno Rezende <brunoc.rez@gmail.com>
2021-02-26 17:52:57 -0300

    creating countdown context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);  
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleFailChallenge() {
+    resetChallenge();
+    resetCountdown();
+  }
+
+  function handleSuccessChallenge() {
+    completeChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -20,12 +32,13 @@ export function ChallengeBox() {
             <button 
               type="button"
               className={styles.failButton}
-              onClick={resetChallenge}>
+              onClick={handleFailChallenge}>
                 Falhei
             </button>
             <button 
               type="button"
-              className={styles.successButton}>
+              className={styles.successButton}
+              onClick={handleSuccessChallenge}>
                 Completei
             </button>
           </footer>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,13 +1,11 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
-
 interface Challenge {
   type: string;
   description: string;
   amount: number;
 }
-
-interface ChallengesContext {
+interface ChallengesContextData {
   level: number;
   currentExperience: number;
   experienceToNextLevel: number;
@@ -16,13 +14,13 @@ interface ChallengesContext {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallenge: () => void;
 }
-
 interface ChallengesProviderProps {
   children: ReactNode;
 }
 
-export const ChallengesContext = createContext({} as ChallengesContext);
+export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
@@ -33,6 +31,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -42,10 +44,37 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomIndex];
 
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission == 'granted') {
+      new Notification('Novo Desafio 😍'), {
+        body: `Valendo ${challenge.amount} XP!`
+      }
+    }
   }
 
   function resetChallenge() {
     setActiveChallenge(null);
+  }
+
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setActiveChallenge(null);
+    setCurrentExperience(finalExperience);
+    setChallengesCompleted(challengesCompleted + 1);
   }
   
   return (
@@ -58,8 +87,9 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         levelUp,
         startNewChallenge,
         activeChallenge,
-        resetChallenge }}>
-      {children}
+        resetChallenge,
+        completeChallenge }}>
+      { children }
     </ChallengesContext.Provider>
   );
 }
