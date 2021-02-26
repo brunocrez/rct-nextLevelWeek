51b013cfe749cb1d4b760eae6023b91deb457e0c
Bruno Rezende <brunoc.rez@gmail.com>
2021-02-26 15:05:26 -0300

    using context api to share components info

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,22 +1,26 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);  
 
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 XP!</header>
+          <header>Ganhe {activeChallenge.amount} XP!</header>
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg`} />
             <strong>Novo Desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
           <footer>
             <button 
               type="button"
-              className={styles.failButton}>
+              className={styles.failButton}
+              onClick={resetChallenge}>
                 Falhei
             </button>
             <button 
